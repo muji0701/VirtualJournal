7d31db0d82be44fc80594fd88fb851101689891f
Muji <765886132@qq.com>
2022-12-05 07:48:21 -0800

    update

--- Server/controllers/wearableControllers.js
+++ Server/controllers/wearableControllers.js
@@ -1,11 +1,35 @@
 const Wearable = require('../models/wearableModel');
 const mongoose = require('mongoose');
-
+const map_hr = new Map();
+const map_step = new Map();
+const map_GPS = new Map();
 // get all wearable data
 const getWearables = async (req, res) => {
-  const wearables = await Wearable.find({}).sort({createdAt: -1});
+  console.log('req',req);
+  const HR = req.headers.hr;
+  const step = req.headers.step;
+  const GPS = req.headers.gps;
+  
+  const HR_data = JSON.parse(HR);
+  const step_data = JSON.parse(step);
+  const GPS_data = JSON.parse(GPS);
 
-  res.status(200).json(wearables);
+  Object.keys(HR_data).forEach(key => {
+    classifyHRByTime(key, Number(HR_data[key]))
+  });
+  Object.keys(step_data).forEach(key => {
+    classifyStepByTime(key, Number(step_data[key]))
+  });
+  console.log('map_hr', map_hr);
+  console.log('map_step', map_step);
+
+const request = require('request');
+
+request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.8753%2C-117.220438&rankby=distance&key=AIzaSyA5K9xIog3RQb55NagkFPOd0LXT2P5T3vY', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log('results', res.body.results);
+});
+  res.status(200).send('success');
 }
 
 //get a single wearable data
@@ -34,6 +58,36 @@ const createWearable = async (req, res) => {
     res.status(200).json(wearable);
   } catch(error) {
     res.status(400).json({error: error.message});
+  }
+}
+
+const classifyHRByTime = (dateAndTime, num) => {
+  const words = dateAndTime.split(' ');
+  const time = words[3];
+  const arr = time.split(':');
+  const hour = arr[0];
+  if(map_hr.has(hour)) {
+    const list = map_hr.get(hour);
+    list[1] = list[1]+1;
+    list[0] = (list[0]*(list[1]-1)+num)/list[1];
+    map_hr.set(hour, list);
+  }else {
+    map_hr.set(hour, [num, 1]);
+  }
+}
+
+const classifyStepByTime = (dateAndTime, num) => {
+  const words = dateAndTime.split(' ');
+  const time = words[3];
+  const arr = time.split(':');
+  const hour = arr[0];
+  if(map_step.has(hour)) {
+    const list = map_step.get(hour);
+    list[1] = list[1]+1;
+    list[0] = (list[0]*(list[1]-1)+num)/list[1];
+    map_step.set(hour, list);
+  }else {
+    map_step.set(hour, [num, 1]);
   }
 }
 
